765fd6a48ddb4639e2521e2235f2e5b92ca0bfbf
Raul <raul.de.onate@gmail.com>
2021-11-11 07:15:42 +0100

    BinaryTreeLevelOrderTraversal. LongestConsecutiveSequence

--- LongestConsecutiveSequence.playground/Contents.swift
+++ LongestConsecutiveSequence.playground/Contents.swift
@@ -40,7 +40,7 @@ func longestConsecutive(_ nums: [Int]) -> Int {
     return maxLength
 
 /*
-    Time complexity : O(n)O.
+    Time complexity : O(n).
 
     Although the time complexity appears to be quadratic due to the while loop nested within the for loop, closer inspection reveals it to be linear. Because the while loop is reached only when currentNum marks the beginning of a sequence (i.e. currentNum-1 is not present in nums), the while loop can only run for nnn iterations throughout the entire runtime of the algorithm. This means that despite looking like O(n⋅n) complexity, the nested loops actually run in O(n+n)=O(n) time. All other computations occur in constant time, so the overall runtime is linear.
 
@@ -73,3 +73,25 @@ class TestCase: XCTestCase {
 }
 
 TestCase.defaultTestSuite.run()
+
+ class HashMapSolution {
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    func longestConsecutive(_ nums: [Int]) -> Int {
+         var countMap = [Int: Int]()
+         var result = 0
+         for num in nums {
+             if countMap[num] == nil {
+                 let leftSum = countMap[num - 1] ?? 0
+                 let rightSum = countMap[num + 1] ?? 0
+                 let streak = leftSum + rightSum + 1
+                 countMap[num] = streak
+                 if leftSum != 0 { countMap[num-leftSum] = streak }
+                 if rightSum != 0 { countMap[num+rightSum] = streak }
+                 result = max(result, streak)
+
+             }
+         }
+         return result
+     }
+ }
